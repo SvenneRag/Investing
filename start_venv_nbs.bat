4bc815c05b58376ee04000cbd3f6008338b4d015
Svend Andreasen <svend.andreasen@gmail.com>
2026-09-14 16:27:25 +0200

    setup of venv, etc

---
+++ start_venv_nbs.bat
@@ -0,0 +1,5 @@
+REM activate virtual environment
+CALL env_inv\Scripts\activate.bat
+
+REM Run Jupyter Notebook
+python -m jupyterlab
--- venv_make.bat
+++ venv_make.bat
@@ -2,5 +2,11 @@
 REM Create virtual environment
 python -m venv env_inv
 
+REM Set environment variables for the virtual environment
+
 
 REM Update pip, setup tools
+pip install --upgrade pip setuptools wheel
+
+REM install required packages
+pip install -r requirements.txt
